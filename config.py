a70179115fdd8585730ca2c9dd4a4e1f1b7b8faf
StanMorrison <stan@StanMorrisons-iMac.local>
2019-01-09 17:41:47 -0700

    First fully functional Cesium map

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 class Config(object):
     MYSQL_DATABASE_DB = 'uranium_database'
     MYSQL_DATABASE_HOST = 'localhost'
+    DEBUG = False
 
 class DevelopmentConfig(Config):
     DEBUG = True
 
 class ProductionConfig(Config):
-    DEBUG = False
+    MYSQL_DATABASE_DB = 'uranium_database'
 
 app_config = {
     'development': DevelopmentConfig,
