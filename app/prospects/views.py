3689d113f31eb064cd76da694084c84c6310056c
StanMorrison <stan@StanMorrisons-iMac.local>
2019-01-13 20:16:41 -0700

    Added Gypsum Valley prospect page

--- app/prospects/views.py
+++ app/prospects/views.py
@@ -1,6 +1,10 @@
 from flask import render_template
 from . import prospects
 
-@prospects.route('/the_prospects')
+@prospects.route('/prospects/the_prospects')
 def the_prospects():
-    return render_template('prospects/the_prospects.html')
+    return render_template('/prospects/the_prospects.html')
+
+@prospects.route('/prospects/GVMS')
+def GVMS():
+    return render_template('/prospects/GVMS.html')
