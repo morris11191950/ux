bd0a65a6cfeb9f48517a78b943cf70f21149eeaa
StanMorrison <stan@StanMorrisons-iMac.local>
2019-05-11 12:37:49 -0600

    Added show map lables to user options modal

--- app/maps/views.py
+++ app/maps/views.py
@@ -6,8 +6,13 @@ from . import maps
 @maps.route('/deposits_by_district/<int:district_id>')
 def deposits_by_district(district_id):
     rows = Queries().deposits_by_district(district_id)
+
+    #print('maps views: rows ', rows, type(rows))
+    # rows.append({'deposit_id': 999999, 'deposit_name': 'Standard Tunnel', 'latitude': 38.18719, 'longitude': -109.25929, 'state': 'Utah', 'country': 'United States', 'county': 'San Juan', 'district_name': 'Lisbon Valley District', 'pounds_u3o8': None, 'grade': None, 'database_name': 'sjm'})
     jsonStr = json.dumps(rows)
+    #print('maps views: jsonStr ', jsonStr, type(jsonStr))
     j = jsonify(Deposits=jsonStr)
+    #print('maps views: j ', j, type(j))
     #print('j in views ', rows)
     return j
 
@@ -40,6 +45,8 @@ def deposits_by_deposit(selected_id_str):
 
 @maps.route('/maps_home')
 def maps_home():
+    #print('In Views: maps home 1')
     cesium_api_key = current_app.config['CESIUM_API_KEY']
+    #print('In Views: maps home 2')
     return render_template('maps/maps_home.html', CESIUM_API_KEY=cesium_api_key)
     #return render_template('maps/maps_home.html',)
--- app/options/views.py
+++ app/options/views.py
@@ -2,45 +2,36 @@ from flask import render_template, jsonify, json, request, session
 from ..models import Queries
 from . import options
 
-@options.route('/options_get_current_dbs')
-def options_get_current_dbs():
-    # print('I am here in get_current')
-    usrDatabases = session.get('usrDatabases')
-    #print('options_get_current_dbs usrDatabases', usrDatabases)
-    #print('options_get_current_dbs usrDatabases TYPE: usrDatabases', type(usrDatabases))
-    if usrDatabases is None:
-         usrDatabases = ['SJM']
-         #print('get_current y', y, ' ', type(y))
-    session['usrDatabases'] = usrDatabases
-    #print('options_get_current_db out usrDatabases', usrDatabases)
-    #print('options_get_current_db out TYPE', type(usrDatabases))
-    return jsonify(usrDatabases)
-
 # PUT THIS AT BOTTOM!
 @options.route('/options_home', methods=(['GET','POST']))
 def options_home():
 
     if request.method == 'GET':
-        #print('Views: Get')
         usrDatabases = session.get('usrDatabases')
-        #print('options_home GET (usrDatabases)', usrDatabases)
-        #print('options_home GET TYPE', type(usrDatabases))
+        usrMapLabels_yn = session.get('usrMapLabels_yn')
+
+        if usrMapLabels_yn is None:
+            usrMapLabels_yn = 'n'
+
         if usrDatabases is None:
             usrDatabases = ['SJM']
-        # else:
-        #     session['usrDatabases'] = json.dumps(usrDatabases)
+
+        session['usrMaplabels_yn'] = usrMapLabels_yn
         session['usrDatabases'] = usrDatabases
-        #print('options_home GET usrDatabases: ', usrDatabases)
-        #print('options_home TYPE ', type(usrDatabases))
-        return jsonify(usrDatabases)
+
+        options = {"usrDatabases": usrDatabases, "usrMapLabels_yn" : usrMapLabels_yn}
+        #print('options_home GET options: ', options, type(options))
+
+        return jsonify(options)
 
     if request.method == 'POST':
-        req_data = request.get_json()
-        #print('Views: 1', session['usrDatabases'])
-        session['usrDatabases'] = req_data
-        #print('options_home Views: post (session[usrDatabases])', session['usrDatabases'])
-        #print('options_home Type: ', type(session['usrDatabases']))
-        #I really have no idea why this works
+
+        data = request.get_json()
+        usrDatabases = data['usrDatabases']
+        usrMapLabels_yn = data['usrMapLabels_yn']
+
+        session['usrDatabases'] = usrDatabases
+        session['usrMapLabels_yn'] = usrMapLabels_yn
+
         resp = jsonify(success=True)
-        #print(resp, type(resp))
         return resp
